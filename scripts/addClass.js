4d6f3046eb5628614f3aacebc47b9def1c78f6bd
sekliheng <sekliheng3@gmail.com>
2021-11-09 11:39:47 +0700

    I have just deleted basic pay in input form

--- scripts/addClass.js
+++ scripts/addClass.js
@@ -1,7 +1,6 @@
     function addRow(){
         var _class = document.getElementById('class').value;
         var _basic = document.getElementById('basic').value;
-        var _salary = document.getElementById('salary').value;
         var _t_all = document.getElementById('t_all').value;
         var _m_all = document.getElementById('m_all').value;
         var _w_all = document.getElementById('w_all').value;
@@ -15,14 +14,12 @@
         var cel3 = newRow.insertCell(2);
         var cel4 = newRow.insertCell(3);
         var cel5 = newRow.insertCell(4);
-        var cel6 = newRow.insertCell(5);
 
         cel1.innerHTML = _class;
         cel2.innerHTML = _basic + '$';
-        cel3.innerHTML = _salary + '$';
-        cel4.innerHTML = _t_all + '$';
-        cel5.innerHTML = _m_all + '$';
-        cel6.innerHTML = _w_all + '$';
+        cel3.innerHTML = _t_all + '$';
+        cel4.innerHTML = _m_all + '$';
+        cel5.innerHTML = _w_all + '$';
 
 
     }
